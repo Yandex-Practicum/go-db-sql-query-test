77f32dc8506dcb4d32e56b3762aa5d4c0d23bbae
Arthur Nurgaliev <iamflyyy@gmail.com>
2024-01-12 00:26:31 +0300

    sprint 8

--- main_test.go
+++ main_test.go
@@ -1,29 +1,86 @@
 package main
 
 import (
+	"database/sql"
+	"errors"
+	"fmt"
 	"testing"
 
 	_ "modernc.org/sqlite"
 )
 
 func Test_SelectClient_WhenOk(t *testing.T) {
-	// настройте подключение к БД
+	db, err := sql.Open("sqlite", "demo.db")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer db.Close()
 
 	clientID := 1
 
-	// напиши тест здесь
+	client, err := selectClient(db, clientID)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if client.ID != clientID {
+		t.Error("Unequal IDs")
+	}
+	if client.FIO == "" {
+		t.Fatal("empty fio")
+	}
+	if client.Login == "" {
+		t.Fatal("empty login")
+	}
+
+	if client.Birthday == "" {
+		t.Fatal("empty birthday")
+	}
+
+	if client.Email == "" {
+		t.Fatal("empty email")
+	}
 }
 
 func Test_SelectClient_WhenNoClient(t *testing.T) {
-	// настройте подключение к БД
+	db, err := sql.Open("sqlite", "demo.db")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer db.Close()
 
 	clientID := -1
 
-	// напиши тест здесь
+	client, err := selectClient(db, clientID)
+	if !errors.Is(err, sql.ErrNoRows) {
+		t.Fatal(err)
+	}
+	if client.ID != 0 {
+		t.Fatal("not empty ID")
+	}
+	if client.FIO != "" {
+		t.Fatal("not empty fio")
+	}
+	if client.Login != "" {
+		t.Fatal("not empty login")
+	}
+
+	if client.Birthday != "" {
+		t.Fatal("not empty birthday")
+	}
+
+	if client.Email != "" {
+		t.Fatal("not empty email")
+	}
 }
 
 func Test_InsertClient_ThenSelectAndCheck(t *testing.T) {
-	// настройте подключение к БД
+	db, err := sql.Open("sqlite", "demo.db")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer db.Close()
 
 	cl := Client{
 		FIO:      "Test",
@@ -32,11 +89,39 @@ func Test_InsertClient_ThenSelectAndCheck(t *testing.T) {
 		Email:    "mail@mail.com",
 	}
 
-	// напиши тест здесь
+	id, err := insertClient(db, cl)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if id == 0 {
+		t.Fatal("empty id")
+	}
+	client, err := selectClient(db, id)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if cl.FIO != client.FIO {
+		t.Fatal("not equal clients")
+	}
+	if cl.Login != client.Login {
+		t.Fatal("not equal clients")
+	}
+	if cl.Email != client.Email {
+		t.Fatal("not equal clients")
+	}
+	if cl.Birthday != client.Birthday {
+		t.Fatal("not equal clients")
+	}
 }
 
 func Test_InsertClient_DeleteClient_ThenCheck(t *testing.T) {
-	// настройте подключение к БД
+	db, err := sql.Open("sqlite", "demo.db")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer db.Close()
 
 	cl := Client{
 		FIO:      "Test",
@@ -45,5 +130,23 @@ func Test_InsertClient_DeleteClient_ThenCheck(t *testing.T) {
 		Email:    "mail@mail.com",
 	}
 
-	// напиши тест здесь
+	id, err := insertClient(db, cl)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if id == 0 {
+		t.Fatal("empty id")
+	}
+	_, err = selectClient(db, id)
+	if err != nil {
+		t.Fatal(err)
+	}
+	err = deleteClient(db, id)
+	if err != nil {
+		t.Fatal(err)
+	}
+	_, err = selectClient(db, id)
+	if !errors.Is(err, sql.ErrNoRows) {
+		t.Fatal(err)
+	}
 }
